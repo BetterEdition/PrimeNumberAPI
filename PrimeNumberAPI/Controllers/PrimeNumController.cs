2effb28d52c611423b5af3a0fecd9003887fe662
Jesper <jesperenemark@hotmail.com>
2019-03-06 13:14:44 +0100

    Webapi for prime numbers

---
+++ PrimeNumberAPI/Controllers/PrimeNumController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PrimeNumberAPI.Model;
+namespace PrimeNumberAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrimeNumController : ControllerBase
+    {
+        PrimeInfo PI = new PrimeInfo();
+
+        [Route("{num}")]
+        public ActionResult<bool> Get(long num)
+        {
+            return PI.IsPrime(num);
+        }
+
+        // POST: api/Prime
+        [Route("{num1}/{num2}")]
+        public ActionResult<long> getToFrom(long num1, long num2)
+        {
+            return PI.getPrimes(num1, num2);
+        }
+
+        // POST: api/PrimeNum
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/PrimeNum/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ PrimeNumberAPI/Model/PrimeInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeNumberAPI.Model
+{
+    public class PrimeInfo
+    {
+
+        public bool IsPrime(long num)
+        {
+            if (num <= 1) return false;
+            if (num == 2) return true;
+            if (num % 2 == 0) return false;
+
+
+            var boundary = (int)Math.Floor(Math.Sqrt(num));
+
+            for (int i = 3; i <= boundary; i += 2)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+
+        public long getPrimes(long num1, long num2)
+        {
+            int primes = 0;
+            for (long i = num1; i <= num2; i++)
+            {
+                if (IsPrime(i))
+                {
+                    primes++;
+                }
+            };
+            return primes;
+        }
+    }
+}
